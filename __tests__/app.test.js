9b04746affe0e60d0e5e750875b78151a12cfc3a
Briseida Pagador <bpagador@gmail.com>
2020-06-08 15:19:06 -0700

    show passing POST route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,21 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('handle request routes ', () => {
-  it('shows plain text reading hello', () => {
+  it('shows plain text', () => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual('hello');
       });
   });
+
+  it('shows echo', () => {
+    return request(app)
+      .post('/echo')
+      .send('hello')
+      .then(res => {
+        expect(res.text).toEqual('hello');
+      });
+  });
+//   it('');
 });
